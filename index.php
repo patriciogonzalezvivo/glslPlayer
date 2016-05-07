81938f77c805723118b33ffbdcc5d4d94e8098f6
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-05-07 15:23:08 -0400

    test

--- index.php
+++ index.php
@@ -27,25 +27,6 @@
     }
 ?>
 
-        <!— Twitter Card—>
-        <meta name="twitter:card" content="video">
-        <meta name="twitter:site" content="@bookofshaders">
-        <meta name="twitter:title" content="GLSL Shader">
-        <meta name="twitter:description" content="The Book of Shaders player">
-        <meta name="twitter:domain" content="thebookofshaders.com">
-<?php
-    if (!empty($_GET['log'])) {
-        echo '
-        <meta name="twitter:url" content="http://player.thebookofshaders.com/?log='. $_GET['log'].'"/>
-        <meta name="twitter:image" content="https://thebookofshaders.com/log/'.$_GET['log'].'.png"/>
-        <meta name="twitter:image:width" content="500">
-        <meta name="twitter:image:height" content="500">
-        
-        <meta name="twitter:player" content="https://thebookofshaders.com/glslPlayer/?log='. $_GET['log'].'">
-        <meta name="twitter:player:width" content="500">
-        <meta name="twitter:player:height" content="500">';
-    }
-?>
 
         <style>
             body {
